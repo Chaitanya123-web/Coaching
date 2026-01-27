897cc6dfae1f428c50b2c1c9889303275c08c67a
Chaitanya Bishnoi <chaitanyabishnoi123@gmail.com>
2026-01-27 22:49:32 +0530

    changed vercel.json

--- src/services/api.js
+++ src/services/api.js
@@ -1,34 +1,23 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: import.meta.env.VITE_API_URL, 
   headers: {
     "Content-Type": "application/json",
   },
 });
 
-// attach jwt token automatically
-api.interceptors.request.use(
-  (config) => {
-    const token = localStorage.getItem("token");
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
-    }
-    return config;
-  },
-  (error) => Promise.reject(error)
-);
-
-// global response & error handler
-api.interceptors.response.use(
-  (response) => response.data,
-  (error) => {
-    const message =
-      error?.response?.data?.message ||
-      error?.message ||
-      "something went wrong";
-    return Promise.reject(message);
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
   }
+  return config;
+});
+
+api.interceptors.response.use(
+  (res) => res.data,
+  (err) => Promise.reject(err)
 );
 
 export default api;
