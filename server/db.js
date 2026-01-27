5c7462991c81463cb2981d8b1891fb68337d07b5
Chaitanya Bishnoi <chaitanyabishnoi123@gmail.com>
2026-01-27 19:46:24 +0530

    added mongodb atlas

--- server/db.js
+++ server/db.js
@@ -2,10 +2,19 @@ import mongoose from "mongoose";
 
 const connectdb = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
-    console.log("mongodb connected");
+    const uri =
+      process.env.USE_CLOUD_DB === "true"
+        ? process.env.MONGO_CLOUD
+        : process.env.MONGO_LOCAL;
+
+    await mongoose.connect(uri);
+    console.log(
+      `✅ MongoDB connected → ${
+        process.env.USE_CLOUD_DB === "true" ? "CLOUD (Atlas)" : "LOCAL (Compass)"
+      }`
+    );
   } catch (err) {
-    console.error("mongodb connection failed:", err.message);
+    console.error("❌ MongoDB connection failed", err);
     process.exit(1);
   }
 };
