5eb9b65cd65e53a425b6f10a296db434730684e8
Chaitanya Bishnoi <chaitanyabishnoi123@gmail.com>
2026-01-30 18:34:44 +0530

    changed vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,12 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-});
+  // Ensure base is just a slash
+  base: '/',
+  build: {
+    // Increase chunk size limit to prevent minification drops
+    chunkSizeWarningLimit: 1600,
+  }
+})
