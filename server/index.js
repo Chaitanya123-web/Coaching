9b69498d2e9415f3cd95c44ad31686d66280d49a
Chaitanya Bishnoi <chaitanyabishnoi123@gmail.com>
2026-02-13 23:07:22 +0530

    changed video.jsx

--- server/index.js
+++ server/index.js
@@ -25,6 +25,7 @@ app.use(
       "http://localhost:5173",
       "https://theindofrenchias.vercel.app",
       "https://theindofrenchias.netlify.app",
+      
       /\.vercel\.app$/
     ],
     credentials: true,
