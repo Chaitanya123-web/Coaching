133178431c7c6e43aa1127c69c4906e38d038c60
Chaitanya Bishnoi <chaitanyabishnoi123@gmail.com>
2026-03-13 18:39:59 +0530

    debugging forgot password

--- server/utils/sendemail.js
+++ server/utils/sendemail.js
@@ -15,6 +15,14 @@ const sendEmail = async ({ to, subject, html }) => {
     socketTimeout: 15000,
   });
 
+  transporter.verify(function (error, success) {
+    if (error) {
+      console.log("Transporter error:", error);
+    } else {
+      console.log("Server is ready to take our messages");
+    }
+  });
+
   const mailOptions = {
     from: `"Indofrench Support" <${process.env.EMAIL_USER}>`,
     to,
