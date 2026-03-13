2e048962234cfda3acd21d61ca71d2f087379944
Chaitanya Bishnoi <chaitanyabishnoi123@gmail.com>
2026-03-13 16:57:59 +0530

    debbuging forgot password

--- server/utils/sendemail.js
+++ server/utils/sendemail.js
@@ -4,12 +4,15 @@ const sendEmail = async ({ to, subject, html }) => {
   const transporter = nodemailer.createTransport({
     host: "smtp.gmail.com",
     port: 587, 
-    secure: false,
-    service: "gmail", 
+    secure: false, 
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS, 
     },
+
+    connectionTimeout: 10000,
+    greetingTimeout: 10000,
+    socketTimeout: 15000,
   });
 
   const mailOptions = {
